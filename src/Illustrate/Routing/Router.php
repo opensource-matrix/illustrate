b6167d5fc8db9bd4b300542ee723c4c2b3513627
Zack <mcodikid@gmail.com>
2019-09-02 09:13:57 -0600

    Automatic Update 480c955f6e414bc8371d2e1a8b61d5e185cfd910

---
+++ src/Illustrate/Routing/Router.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Illustrate\Router;
+
+use Illustrate\Contracts\Routing\Router as RouterContract;
+
+class Router implements RouterContract {
+    /**
+     * The path that was called.
+     * 
+     * @var string
+     */
+    public $path;
+
+    public function evaluate($path = '/') {
+        $this->$path = $path;
+
+        if($_SERVER['REQUEST_METHOD'] == 'GET') {
+            $this->get();
+        }
+    }
+
+    private function get() {
+        
+    }
+}
