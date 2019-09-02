13c34cf6d65e42fa4ecad0bbdce77bb0f23840b8
Zack <mcodikid@gmail.com>
2019-09-02 11:02:09 -0600

    Automatic Update f0bad29fde3885a86b0dc6d203898778768e85d0

--- src/Illustrate/Auth/Events/Lockout.php
+++ src/Illustrate/Auth/Events/Lockout.php
@@ -1,18 +1,18 @@
 <?php
-namespace Illuminate\Auth\Events;
-use Illuminate\Http\Request;
+namespace Illustrate\Auth\Events;
+use Illustrate\Http\Request;
 class Lockout
 {
     /**
      * The throttled request.
      *
-     * @var \Illuminate\Http\Request
+     * @var \Illustrate\Http\Request
      */
     public $request;
     /**
      * Create a new event instance.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illustrate\Http\Request  $request
      * @return void
      */
     public function __construct(Request $request)
