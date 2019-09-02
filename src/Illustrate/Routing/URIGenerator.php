b158aebae3f13c2a84fd7d8afdf059ffc308c0f0
Zack <mcodikid@gmail.com>
2019-09-02 09:27:49 -0600

    Automatic Update 480c955f6e414bc8371d2e1a8b61d5e185cfd910

--- src/Illustrate/Routing/URIGenerator.php
+++ src/Illustrate/Routing/URIGenerator.php
@@ -5,6 +5,6 @@ namespace Illustrate\Routing;
 use Illustrate\Contracts\Routing\URIGenerator as URIGeneratorContract;
 // \{(.*|[a-zA-Z0-9]*)\}
 
-class URIGenerator extends URIGeneratorContract {
+class URIGenerator implements URIGeneratorContract {
     
 }
