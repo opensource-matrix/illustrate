19ece51d93a5146fa02c0ceb6c652c78b63d77a4
Zack <mcodikid@gmail.com>
2019-09-02 09:30:04 -0600

    Automatic Update 480c955f6e414bc8371d2e1a8b61d5e185cfd910

--- src/Illustrate/Contracts/Routing/URIGenerator.php
+++ src/Illustrate/Contracts/Routing/URIGenerator.php
@@ -5,6 +5,8 @@ namespace Illustrate\Contracts\Routing;
 interface URIGenerator {
     /**
      * Set the path to generate the URI regex from.
+     * 
+     * @param $path;
      */
     public function setPath($path);
 }
