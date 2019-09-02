856c6ba2d5fb92fd291128398de5fbf7d89e86b6
Zack <mcodikid@gmail.com>
2019-09-02 09:11:43 -0600

    Automatic Update 480c955f6e414bc8371d2e1a8b61d5e185cfd910

--- src/Illustrate/Routing/Router.php
+++ src/Illustrate/Routing/Router.php
@@ -5,6 +5,9 @@ namespace Illustrate\Router;
 use Illustrate\Contracts\Routing\Router as RouterContract;
 
 class Router implements RouterContract {
+    /**
+     * 
+     */
     public $path;
 
     public function evaluate($path = '/') {
