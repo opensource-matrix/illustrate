16fcf9e4a389b192969512ed4e0c88bb050abc7a
Zack <mcodikid@gmail.com>
2019-09-02 12:13:00 -0600

    Initialize oop.

--- src/Illustrate/Contracts/Foundation/Application.php
+++ src/Illustrate/Contracts/Foundation/Application.php
@@ -3,5 +3,20 @@
 namespace Illustrate\Contracts\Foundation;
 
 interface Application {
-    
+    /**
+     * Call the a function and cache it under the current application.
+     * 
+     * @param Closure $closure
+     * @param mixed $...
+     * @return void
+     */
+    public function call();
+
+    /**
+     * Cache an array of data.
+     * 
+     * @param array $data
+     * @return void
+     */
+    public function cache($data);
 }
