81e1049abd3c6563b52949032a35450d38f92dad
Zack <mcodikid@gmail.com>
2019-09-02 11:05:30 -0600

    Automatic Update f0bad29fde3885a86b0dc6d203898778768e85d0

--- src/Illustrate/Auth/Events/Verified.php
+++ src/Illustrate/Auth/Events/Verified.php
@@ -1,23 +1,21 @@
 <?php
-namespace Illuminate\Auth\Events;
-use Illuminate\Queue\SerializesModels;
-class Verified
-{
+namespace Illustrate\Auth\Events;
+use Illustrate\Queue\SerializesModels;
+class Verified {
     use SerializesModels;
     /**
      * The verified user.
      *
-     * @var \Illuminate\Contracts\Auth\MustVerifyEmail
+     * @var \Illustrate\Contracts\Auth\MustVerifyEmail
      */
     public $user;
     /**
      * Create a new event instance.
      *
-     * @param  \Illuminate\Contracts\Auth\MustVerifyEmail  $user
+     * @param  \Illustrate\Contracts\Auth\MustVerifyEmail  $user
      * @return void
      */
-    public function __construct($user)
-    {
+    public function __construct($user) {
         $this->user = $user;
     }
 }
